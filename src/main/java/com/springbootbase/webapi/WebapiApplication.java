955dbc683b14ec11f01151f755e2ca2191981ea2
Gio <rodriguez_carlos@bah.com>
2022-10-25 19:04:25 -0400

    Swagger configuration

--- src/main/java/com/springbootbase/webapi/WebapiApplication.java
+++ src/main/java/com/springbootbase/webapi/WebapiApplication.java
@@ -3,7 +3,15 @@ package com.springbootbase.webapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+	title = "Base Spring Boot API",
+	version = "1.0",
+	description = "Base Spring Boot API that is used to collect dependencies for a base project"
+))
 public class WebapiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/springbootbase/webapi/controller/BaseController.java
+++ src/main/java/com/springbootbase/webapi/controller/BaseController.java
@@ -12,12 +12,27 @@ import com.springbootbase.webapi.model.ApiResponse;
 import com.springbootbase.webapi.model.VersionModel;
 import com.springbootbase.webapi.service.BaseService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 @RestController
 public class BaseController {
 	
 	@Autowired
 	private BaseService baseService;
 
+	@Operation(summary = "Get assembly version information")
+	@ApiResponses(value = {
+		@io.swagger.v3.oas.annotations.responses.ApiResponse(
+			responseCode = "200",
+			description = "Version information",
+			content = { @Content(mediaType="application/json",
+				schema = @Schema(implementation = VersionModel.class))
+			}
+		)
+	})
 	@GetMapping(value = "/v1/version", headers = "Accept=application/json", produces = "application/json")
 	public ResponseEntity<ApiResponse<VersionModel>> version(HttpServletRequest request) {
 		ApiResponse<VersionModel> apiResponse = baseService.version(request);
